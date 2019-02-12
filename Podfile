cbd7ea7407685e23347d7d2bd90aa6d8a62ba31d
Wasith Theerapattrathamrong <phai@live.com>
2019-02-12 18:21:16 +0700

    Another commit

--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+platform :ios, '12.1'
 
 target 'SNAP' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
@@ -9,7 +9,6 @@ target 'SNAP' do
 
   target 'SNAPTests' do
     inherit! :search_paths
-    use_frameworks!
     # Pods for testing
     pod 'iOSSnapshotTestCase'
   end
--- SNAP/FirstViewControllerTests.swift
+++ SNAP/FirstViewControllerTests.swift
@@ -1,19 +1,9 @@
 import XCTest
 @testable import SNAP
 
-class FirstViewControllerTests: TestCase {
-
-  override func setUp() {
-    recordMode = true
-  }
+class FirstViewControllerTests: XCTest {
 
   func test_firstViewController() {
-    let storyboard = UIStoryboard(
-      name: "First",
-      bundle: Bundle.init(identifier: "SNAP"))
-    let viewContoller = storyboard
-      .instantiateViewController(withIdentifier: "FirstViewController")
-
-    FBSnapshotVerifyView(viewContoller.view)
+    XCTAssertEqual("", "2")
   }
 }
