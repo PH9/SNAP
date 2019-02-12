9c740a8c84f6ecdba27f40b56cd6323dbe1a1709
Wasith Theerapattrathamrong <phai@live.com>
2019-02-12 17:29:29 +0700

    Remove comment

--- SNAP/SecondViewController.swift
+++ SNAP/SecondViewController.swift
@@ -1,20 +1,5 @@
-//
-//  SecondViewController.swift
-//  SNAP
-//
-//  Created by Wasith Theerapattrathamrong on 12/2/2562 BE.
-//  Copyright © 2562 Wasith Theerapattrathamrong. All rights reserved.
-//
-
 import UIKit
 
 class SecondViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-
 }
 
