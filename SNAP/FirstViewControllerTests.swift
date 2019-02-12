5a5771ac0beb58bb7aeaada9ba1bfd91b5f6a2c6
Wasith Theerapattrathamrong <phai@live.com>
2019-02-12 18:04:03 +0700

    Just commit

--- SNAP/FirstViewControllerTests.swift
+++ SNAP/FirstViewControllerTests.swift
@@ -1,3 +1,16 @@
+import XCTest
+@testable import SNAP
+
 class FirstViewControllerTests: TestCase {
 
+  func test_firstViewController() {
+    recordMode = true
+
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    let identifier = String(describing: FirstViewController.self)
+    let viewContoller = storyboard
+      .instantiateViewController(withIdentifier: identifier)
+
+    FBSnapshotVerifyView(viewContoller.view)
+  }
 }
