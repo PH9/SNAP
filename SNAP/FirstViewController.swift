a0b99d52980f0c3225110c41a7964928f177a623
Wasith Theerapattrathamrong <phai@live.com>
2019-02-12 17:22:51 +0700

    Initial Commit

---
+++ SNAP/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SNAP
+//
+//  Created by Wasith Theerapattrathamrong on 12/2/2562 BE.
+//  Copyright © 2562 Wasith Theerapattrathamrong. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ SNAP/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SNAP
+//
+//  Created by Wasith Theerapattrathamrong on 12/2/2562 BE.
+//  Copyright © 2562 Wasith Theerapattrathamrong. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
